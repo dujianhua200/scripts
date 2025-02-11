e210bd339f224a93a4bd351849036c33598327d2
dujianhua200 <134572655+dujianhua200@users.noreply.github.com>
2025-02-11 22:36:56 +0800

    Update jj510.sh

--- jj510.sh
+++ jj510.sh
@@ -17,19 +17,61 @@ export UUID=${UUID:-'a6b9ecc2-e22f-42ac-bf72-f6550f0ee55d'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''}     # 哪吒面板域名，哪吒3个变量不全不安装
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     # 哪吒面板通信端口
 export NEZHA_KEY=${NEZHA_KEY:-''}           # 哪吒密钥，端口为{443,8443,2096,2087,2083,2053}其中之一时自动开启tls
-export ARGO_DOMAIN=${ARGO_DOMAIN:-'argo2.hainasi.eu.org'}      # ARGO 固定隧道域名，留空将使用临时隧道
-export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiYmMxNzE2NjViNWM3MjRhZTY4ZjBhYWFiNDhlNDQyNzEiLCJ0IjoiODExZDg0N2QtOTMzYi00NDdiLTkzMjctY2RmODEzOTk3Y2MxIiwicyI6IllXSm1NVE14TldRdFlqRTBaUzAwTldNMUxXRTVOV0V0TWpsbE5XTmhZVFJtTXpnNCJ9'}         # ARGO 固定隧道json或token，留空将使用临时隧道
-export CFIP=${CFIP:-'www.visa.com.tw'}   # 优选ip或优选域名
-export CFPORT=${CFPORT:-'443'}          # 优选ip或优选域名对应端口  
-export PORT=${PORT:-'34105'}           # ARGO端口必填
+export ARGO_DOMAIN=${ARGO_DOMAIN:-'argo2.hainasi.eu.org'}       # ARGO 固定隧道域名，留空将使用临时隧道
+export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiYmMxNzE2NjViNWM3MjRhZTY4ZjBhYWFiNDhlNDQyNzEiLCJ0IjoiODExZDg0N2QtOTMzYi00NDdiLTkzMjctY2RmODEzOTk3Y2MxIiwicyI6IllXSm1NVE14TldRdFlqRTBaUzAwTldNMUxXRTVOV0V0TWpsbE5XTmhZVFJtTXpnNCJ9'}           # ARGO 固定隧道json或token，留空将使用临时隧道
+export CFIP=${CFIP:-'www.visa.com.tw'}      # 优选ip或优选域名
+export CFPORT=${CFPORT:-'443'}              # 优选ip或优选域名对应端口  
+export PORT=${PORT:-'61397'}                     # ARGO端口不填自动获取
+export SUB_TOKEN=${SUB_TOKEN:-'sub'}
+export CHAT_ID=${CHAT_ID:-'6992418262'} 
+export BOT_TOKEN=${BOT_TOKEN:-'6373229598:AAEvykN1CpJNXqTnUpNh7OwUfbXtF_4Rb3w'} 
 
-[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs" && rm -rf $WORKDIR
-[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
-ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="${HOME}/domains/${USERNAME}.ct8.pl/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.ct8.pl/public_html" || WORKDIR="${HOME}/domains/${USERNAME}.serv00.net/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.serv00.net/public_html"
+rm -rf "$WORKDIR" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
+bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
+
+check_binexec_and_port () {
+  purple "正在安装中,请稍等..."
+  port_list=$(devil port list)
+  tcp_ports=$(echo "$port_list" | grep -c "tcp")
+  udp_ports=$(echo "$port_list" | grep -c "udp")
+
+  if [[ $tcp_ports -lt 1 ]]; then
+      red "没有可用的TCP端口,正在调整..."
+
+      if [[ $udp_ports -ge 3 ]]; then
+          udp_port_to_delete=$(echo "$port_list" | awk '/udp/ {print $1}' | head -n 1)
+          devil port del udp $udp_port_to_delete
+          green "已删除udp端口: $udp_port_to_delete"
+      fi
+
+      while true; do
+          tcp_port=$(shuf -i 10000-65535 -n 1)
+          result=$(devil port add tcp $tcp_port 2>&1)
+          if [[ $result == *"succesfully"* ]]; then
+              green "已添加TCP端口: $tcp_port"
+              tcp_port1=$tcp_port
+              break
+          else
+              yellow "端口 $tcp_port 不可用，尝试其他端口..."
+          fi
+      done
+
+      green "端口已调整完成, 将断开SSH连接, 请重新连接SSH并重新执行脚本"
+      devil binexec on >/dev/null 2>&1
+      kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
+  else
+      tcp_ports=$(echo "$port_list" | awk '/tcp/ {print $1}')
+      tcp_port1=$(echo "$tcp_ports" | sed -n '1p')
+
+      purple "当前TCP端口: $tcp_port1"
+  fi
+
+  export PORT=$tcp_port1
+}
+check_binexec_and_port
 
 argo_configure() {
-clear
-purple "正在安装中,请稍等..."
   if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
     green "ARGO_DOMAIN or ARGO_AUTH is empty,use quick tunnel"
     return
@@ -92,9 +134,6 @@ done
 wait
 
 generate_config() {
-  output=$(./"${FILE_MAP[web]}" x25519)
-  private_key=$(echo "$output" | grep "Private key" | cut -d ' ' -f 3)
-  public_key=$(echo "$output" | grep "Public key" | cut -d ' ' -f 3)
   
   cat > config.json << EOF
 {
@@ -119,7 +158,7 @@ generate_config() {
             "streamSettings": {
                 "network": "ws",
                 "wsSettings": {
-                    "path": "/vmess"
+                    "path": "/vmess-argo"
                 }
             }
         }
@@ -129,54 +168,40 @@ generate_config() {
             "https+local://8.8.8.8/dns-query"
         ]
     },
-    "outbounds":[
+    "outbounds": [
         {
-            "protocol":"freedom"
-        },
+          "protocol": "freedom",
+          "tag": "direct"
+          },
         {
-            "tag":"WARP",
-            "protocol":"wireguard",
-            "settings":{
-                "secretKey":"YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
-                "address":[
-                    "172.16.0.2/32",
-                    "2606:4700:110:8a36:df92:102a:9602:fa18/128"
-                ],
-                "peers":[
-                    {
-                        "publicKey":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                        "allowedIPs":[
-                            "0.0.0.0/0",
-                            "::/0"
-                        ],
-                        "endpoint":"162.159.193.10:2408"
-                    }
-                ],
-                "reserved":[78, 135, 76],
-                "mtu":1280
-            }
+          "protocol": "blackhole",
+          "tag": "blocked"
         }
-    ],
-    "routing":{
-        "domainStrategy":"AsIs",
-        "rules":[
-            {
-                "type":"field",
-                "domain":[
-                    "domain:openai.com",
-                    "domain:chatgpt.com",
-                    "domain:auth.openai.com",
-                    "domain:chat.openai.com"
-                ],
-                "outboundTag":"WARP"
-            }
-        ]
-    }
+    ] 
 }
 EOF
 }
 generate_config
 wait
+
+if [ -e "$(basename ${FILE_MAP[web]})" ]; then
+    nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 &
+    sleep 2
+    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && green "$(basename ${FILE_MAP[web]}) is running" || { red "$(basename ${FILE_MAP[web]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[web]}) restarted"; }
+fi
+
+if [ -e "$(basename ${FILE_MAP[bot]})" ]; then
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+      args="tunnel --edge-ip-version auto --config tunnel.yml run"
+    else
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile "${WORKDIR}/boot.log" --loglevel info --url http://localhost:$PORT"
+    fi
+    nohup ./"$(basename ${FILE_MAP[bot]})" $args >/dev/null 2>&1 &
+    sleep 2
+    pgrep -x "$(basename ${FILE_MAP[bot]})" > /dev/null && green "$(basename ${FILE_MAP[bot]}) is running" || { red "$(basename ${FILE_MAP[bot]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[bot]})" && nohup ./"$(basename ${FILE_MAP[bot]})" "${args}" >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[bot]}) restarted"; }
+fi
 
 if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
     tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
@@ -195,56 +220,107 @@ if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
     fi
 fi
 
-if [ -e "$(basename ${FILE_MAP[web]})" ]; then
-    nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 &
-    sleep 2
-    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && green "$(basename ${FILE_MAP[web]}) is running" || { red "$(basename ${FILE_MAP[web]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[web]}) restarted"; }
-fi
-
-if [ -e "$(basename ${FILE_MAP[bot]})" ]; then
-    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
-    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
-      args="tunnel --edge-ip-version auto --config tunnel.yml run"
-    else
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile "${WORKDIR}/boot.log" --loglevel info --url http://localhost:$PORT"
-    fi
-    nohup ./"$(basename ${FILE_MAP[bot]})" $args >/dev/null 2>&1 &
-    sleep 3
-    pgrep -x "$(basename ${FILE_MAP[bot]})" > /dev/null && green "$(basename ${FILE_MAP[bot]}) is running" || { red "$(basename ${FILE_MAP[bot]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[bot]})" && nohup ./"$(basename ${FILE_MAP[bot]})" "${args}" >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[bot]}) restarted"; }
-fi
-sleep 5
+sleep 1
 rm -f "$(basename ${FILE_MAP[npm]})" "$(basename ${FILE_MAP[web]})" "$(basename ${FILE_MAP[bot]})"
 
 get_argodomain() {
   if [[ -n $ARGO_AUTH ]]; then
     echo "$ARGO_DOMAIN"
   else
-    grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' "${WORKDIR}/boot.log" | sed 's@https://@@'
+    local retry=0
+    local max_retries=6
+    local argodomain=""
+    while [[ $retry -lt $max_retries ]]; do
+      ((retry++))
+      argodomain=$(grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' "${WORKDIR}/boot.log" | sed 's@https://@@') 
+      if [[ -n $argodomain ]]; then
+        break
+      fi
+      sleep 1
+    done
+    echo "$argodomain"
   fi
+}
+
+install_keepalive () {
+    purple "正在安装保活服务中,请稍等......"
+    keep_path="$HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs"
+    [ -d "$keep_path" ] || mkdir -p "$keep_path"
+    app_file_url="https://xray.2go.us.kg/app.js"
+
+    if command -v curl &> /dev/null; then
+        curl -s -o "${keep_path}/app.js" "$app_file_url"
+    elif command -v wget &> /dev/null; then
+        wget -q -O "${keep_path}/app.js" "$app_file_url"
+    else
+        echo "警告: 文件下载失败,请手动从https://xray.2go.us.kg/app.js下载文件,并将文件上传到${keep_path}目录下"
+        return
+    fi
+
+    cat > ${keep_path}/.env <<EOF
+UUID=${UUID}
+CFIP=${CFIP}
+CFPORT=${CFPORT}
+SUB_TOKEN=${SUB_TOKEN}
+TELEGRAM_CHAT_ID=${CHAT_ID}
+TELEGRAM_BOT_TOKEN=${BOT_TOKEN}
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
+ARGO_DOMAIN=${ARGO_DOMAIN}
+ARGO_AUTH='${ARGO_AUTH}'
+EOF
+    devil www add ${USERNAME}.serv00.net php > /dev/null 2>&1
+    devil www add keep.${USERNAME}.serv00.net nodejs /usr/local/bin/node18 > /dev/null 2>&1
+    ip_address=$(devil vhost list | sed -n '5p' | awk '{print $1}')
+    devil ssl www add $ip_address le le keep.${USERNAME}.serv00.net > /dev/null 2>&1
+    ln -fs /usr/local/bin/node18 ~/bin/node > /dev/null 2>&1
+    ln -fs /usr/local/bin/npm18 ~/bin/npm > /dev/null 2>&1
+    mkdir -p ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
+    rm -rf $HOME/.npmrc > /dev/null 2>&1
+    cd ${keep_path} && npm install dotenv axios --silent > /dev/null 2>&1
+    rm $HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs/public/index.html > /dev/null 2>&1
+    devil www options keep.${USERNAME}.serv00.net sslonly on > /dev/null 2>&1
+    if devil www restart keep.${USERNAME}.serv00.net 2>&1 | grep -q "succesfully"; then
+        green "\n全自动保活服务安装成功\n"
+        green "========================================================"
+        purple "\n访问 https://keep.${USERNAME}.serv00.net/status 查看进程状态\n"
+        yellow "访问 https://keep.${USERNAME}.serv00.net/start 调起保活程序\n"
+        purple "访问 https://keep.${USERNAME}.serv00.net/list 全部进程列表\n"
+        purple "访问 https://keep.${USERNAME}.serv00.net/stop 结束进程和保活\n"
+        green "========================================================"
+        yellow "如发现掉线访问https://keep.${USERNAME}.serv00.net/start唤醒,或者用https://console.cron-job.org在线访问网页自动唤醒\n"
+        purple "如果需要Telegram通知，请先在Telegram @Botfather 申请 Bot-Token，并带CHAT_ID和BOT_TOKEN环境变量运行\n\n"
+        
+    else
+        red "全自动保活服务安装失败,请删除所有文件夹后重试\n"
+    fi
 }
 
 generate_links() {
   argodomain=$(get_argodomain)
-  echo -e "\e[1;32mArgoDomain:\e[1;35m${argodomain}\e[0m\n"
+  echo -e "\e[1;32mArgoDomain: \e[1;35m${argodomain}\e[0m\n"
   sleep 1
-  isp=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
-  sleep 1
-  cat > ${WORKDIR}/list.txt <<EOF
-vmess://$(echo "{ \"v\": \"2\", \"ps\": \"${isp}\", \"add\": \"${CFIP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }" | base64 -w0)
+  isp=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "00")
+  get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
+  NAME=${isp}-$(get_name)-vmess-argo-${USERNAME}
+  FILE_PATH="/usr/home/${USERNAME}/domains/${USERNAME}.serv00.net/public_html"
+  cat > ${FILE_PATH}/${SUB_TOKEN}_vmess.log <<EOF
+vmess://$(echo "{ \"v\": \"2\", \"ps\": \"${NAME}\", \"add\": \"${CFIP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"vmess-argo?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }" | base64 -w0)
 EOF
-
-  cat ${WORKDIR}/list.txt
-  echo -e "\n\e[1;32m${WORKDIR}/list.txt saved successfully\e[0m"
-  sleep 2  
+  cat ${FILE_PATH}/${SUB_TOKEN}_vmess.log
+  green "\n订阅连接: https://${USERNAME}.serv00.net/${SUB_TOKEN}_vmess.log 适用于V2ranN/Nekobox/Karing/小火箭/sterisand/Loon 等\n" 
   rm -rf config.json fake_useragent_0.2.0.json ${WORKDIR}/boot.log ${WORKDIR}/tunnel.json ${WORKDIR}/tunnel.yml 
+  install_keepalive
 }
 generate_links
-purple "Running done!"
-purple "Thank you for using this script,enjoy!"
+
 yellow "Serv00|ct8老王一键vmess-ws-tls(argo)无交互安装脚本\n"
 echo -e "${green}issues反馈：${re}${yellow}https://github.com/eooce/Sing-box/scrips${re}\n"
 echo -e "${green}反馈论坛：${re}${yellow}https://bbs.vps8.me${re}\n"
 echo -e "${green}TG反馈群组：${re}${yellow}https://t.me/vps888${re}\n"
 purple "转载请保留出处，违者必纠！请勿滥用！！!\n"
-exit 0
+purple "Running done!\n"
+purple "Thank you for using this script,enjoy!\n"
